6791557ec40088a86b938779852f70d23f8092b9
Amanda Iverson <ajohnson764@gmail.com>
2019-03-21 21:06:59 -0700

    update TODOs

--- class04/StarterCode/Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
+++ class04/StarterCode/Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
@@ -8,5 +8,12 @@ namespace Lab04_TicTacToe
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void PlayGame()
+        {
+            // TODO: Setup your game here. Create an introduction. 
+            // Create your players, and instantiate your Game class. 
+            // output to the console the winner
+        }
     }
 }
