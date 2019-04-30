24472fe0a514489c1ac7a2fa32ca102f56e41745
Amanda Iverson <ajohnson764@gmail.com>
2019-04-29 17:30:16 -0700

    Add Demo Code

---
+++ class31/Demo/CMSBlogPost/CMSBlogPost/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CMSBlogPost.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage ="The {0} must be at least {2} characters long, and a max of {1} characters long", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage ="The passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
