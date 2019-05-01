99fbee99cc4e5251863c7d3a9eb85b9e7bfc9601
Amanda Iverson <ajohnson764@gmail.com>
2019-04-30 17:49:46 -0700

    adding class demo code

---
+++ class32/demo/CMSBlogPost/CMSBlogPost/Data/BlogDbContext.cs
@@ -0,0 +1,45 @@
+﻿using CMSBlogPost.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMSBlogPost.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().HasData(
+                new Post
+                {
+                    ID = 1,
+                    Title = "Tacos are Delicious",
+                    Description = "They are the best!"
+                },
+                new Post
+                {
+                    ID = 2,
+                    Title = "Josie Cat",
+                    Description = "Josie is the queen of the household"
+                },
+                new Post
+                {
+                    ID = 3,
+                    Title = "Belle Kitty",
+                    Description = "Belle loves to lay around the house and meow"
+                },
+                new Post
+                {
+                    ID = 4,
+                    Title = ".NET ROCKS",
+                    Description = ".NET is the bestest of the best!! We all love to code in C#!!!!!"
+                }
+                );
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ class32/demo/CMSBlogPost/CMSBlogPost/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSBlogPost.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name ="What is your Email/Username?")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
